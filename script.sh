3a369d5ae382528b05d3183bd614ac28da24e0cd
MatMB115 <MatMB115@users.noreply.github.com>
2023-08-24 17:24:11 -0300

    feat: criando todos usuários e concedendo as permissões

--- script.sh
+++ script.sh
@@ -7,31 +7,41 @@ echo "Criando grupos"
 groupadd GRP_ADM && groupadd GRP_VEN && groupadd GRP_SEC
 
 echo "Criando usuários do grupo ADM"
-useradd carlos -c "Carlos ADM" -G GRP_ADM -p $(openssl passwd senha123)
+useradd carlos -c "Carlos ADM" -G GRP_ADM -p $(openssl passwd senha123) -s /bin/bash
 passwd carlos -e
 
-useradd maria -c "Maria ADM" -G GRP_ADM -p $(openssl passwd senha123)
+useradd maria -c "Maria ADM" -G GRP_ADM -p $(openssl passwd senha123) -s /bin/bash
 passwd maria -e
 
-useradd joao -c "João ADM" -G GRP_ADM -p $(openssl passwd senha123)
+useradd joao -c "João ADM" -G GRP_ADM -p $(openssl passwd senha123) -s /bin/bash
 passwd joao -e
 
 echo "Criando usuários do grupo VEN"
-useradd debora -c "Débora VEN" -G GRP_VEN -p $(openssl passwd senha123)
+useradd debora -c "Débora VEN" -G GRP_VEN -p $(openssl passwd senha123) -s /bin/bash
 passwd debora -e
 
-useradd roberto -c "Roberto VEN" -G GRP_VEN -p $(openssl passwd senha123)
+useradd roberto -c "Roberto VEN" -G GRP_VEN -p $(openssl passwd senha123) -s /bin/bash
 passwd roberto -e
 
-useradd sebastiana -c "Sebastiana VEN" -G GRP_VEN -p $(openssel passwd senha123)
-passwd sebastiana
+useradd sebastiana -c "Sebastiana VEN" -G GRP_VEN -p $(openssl passwd senha123) -s /bin/bash
+passwd sebastiana -e
 
 echo "Criando usuários do grupo SEC"
-useradd josefina -c "Josefina SEC" -G GRP_SEC -p $(openssel passwd senha123)
+useradd josefina -c "Josefina SEC" -G GRP_SEC -p $(openssl passwd senha123) -s /bin/bash
 passwd josefina -e
 
-useradd amanda -c "Amanda SEC" -G GRP_SEC -p $(openssel passwd senha123)
+useradd amanda -c "Amanda SEC" -G GRP_SEC -p $(openssl passwd senha123) -s /bin/bash
 passwd amanda -e
 
-useradd rogerio -c "Rogério SEC" -G GRP_SEC -p $(openssel passwd senha123)
+useradd rogerio -c "Rogério SEC" -G GRP_SEC -p $(openssl passwd senha123) -s /bin/bash
 passwd rogerio -e
+
+echo "Concendendo permissões"
+chown root:GRP_ADM /adm
+chmod 770 /adm
+
+chown root:GRP_VEN /ven
+chmod 770 /ven
+
+chown root:GRP_SEC /sec
+chmod 770 /sec
